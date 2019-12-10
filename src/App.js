baf61de3e4fc521406095f62a5a19031d416693f
David Tauraso <david318@me.com>
2019-12-09 16:51:50 -0800

    Added useLocalStorage hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CartContext";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,7 +14,7 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	// {id : cartItem}
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useLocalStorage('cart', [])//useState([]);
 
 	const addItem = item => {
 		// if(key not in cart) its a new item
